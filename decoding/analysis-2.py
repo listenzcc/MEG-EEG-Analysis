78fb7aba10f117b8b9f86abeb42db48d68b5889e
listenzcc <listenzcc@mail.bnu.edu.cn>
2022-04-27 10:54:08 +0800

    Daily update

---
+++ decoding/analysis-2.py
@@ -0,0 +1,197 @@
+
+# %%
+import os
+import plotly.express as px
+import numpy as np
+from joblib import load
+from pathlib import Path
+from tqdm.auto import tqdm
+
+from sklearn import metrics
+
+import torch
+import skorch
+from skorch.callbacks import LRScheduler
+from skorch.helper import predefined_split
+from braindecode import EEGClassifier
+from braindecode.training import CroppedLoss
+from braindecode.models import ShallowFBCSPNet, EEGNetv4
+
+from braindecode.augmentation import AugmentedDataLoader, SignFlip
+from braindecode.augmentation import FrequencyShift
+
+from onstart import LOGGER
+
+# %%
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+# %%
+subject_name_table = dict(
+    S01='meg-eeg-20220119',
+    S02='MEG20220315',
+    S03='MEG-EEG20220322',
+    S04='MEG20220329'
+)
+
+
+# %%
+def _get_x_y_g(filenames):
+    X, Y, G = [], [], []
+
+    for name in tqdm(filenames):
+        d = load(name)
+        run_idx = int(name.as_posix().split('.')[1])
+        x = d['data'][:, :, 200:]
+        y = d['events'][:, -1]
+        y[y == 5] = 0
+        g = run_idx + y * 0
+
+        X.append(x)
+        Y.append(y)
+        G.append(g)
+
+        ch_names = d['ch_names']
+
+    return np.concatenate(X).astype(np.float64), np.concatenate(Y).astype(np.int64), np.concatenate(G).astype(np.int64), ch_names
+
+
+def _select_mode(X, ch_names, mode='MEG'):
+    select = [True for e in ch_names]
+
+    if mode == 'MEG':
+        select = [not e.startswith('EEG') for e in ch_names]
+
+    if mode == 'EEG':
+        select = [e.startswith('EEG') for e in ch_names]
+
+    x = X[:, select]
+
+    if mode == 'EEG':
+        x = x[:, :35]
+
+    LOGGER.debug('Selected mode {} for {}'.format(mode, x.shape))
+
+    return x
+
+
+def _preprocess(X):
+    for j in range(len(X)):
+        m1 = np.min(X[j])
+        m2 = np.max(X[j])
+        X[j] /= m2
+    return X
+
+
+def decoding(X, Y, G, valid_group):
+    n_classes = 5
+    sfreq = 200
+
+    freq_shift = FrequencyShift(
+        probability=.5,
+        sfreq=sfreq,
+        max_delta_freq=2.  # the frequency shifts are sampled now between -2 and 2 Hz
+    )
+
+    sign_flip = SignFlip(probability=.3)
+
+    transforms = [
+        freq_shift,
+        sign_flip
+    ]
+
+    # These values we found good for shallow network:
+    lr = 0.0625 * 0.1
+    weight_decay = 1.0
+
+    batch_size = 50
+    n_epochs = 500
+
+    n_chans = X.shape[1]
+    input_window_samples = X.shape[2]
+
+    model = ShallowFBCSPNet(
+        n_chans,
+        n_classes,
+        pool_mode='mean',
+        input_window_samples=input_window_samples,
+        final_conv_length='auto',
+    )
+
+    def train_split(dataset, G=G, valid_group=valid_group):
+        X = dataset.X
+        Y = dataset.y
+        train = skorch.dataset.Dataset(
+            X[G != valid_group], Y[G != valid_group])
+        valid = skorch.dataset.Dataset(
+            X[G == valid_group], Y[G == valid_group])
+        return tuple((train, valid))
+
+    clf = EEGClassifier(
+        model,
+        iterator_train=AugmentedDataLoader,
+        # iterator_train__transforms=transforms,
+        cropped=True,
+        criterion=CroppedLoss,
+        criterion__loss_function=torch.nn.functional.nll_loss,
+        optimizer=torch.optim.AdamW,
+        optimizer__lr=lr,
+        optimizer__weight_decay=weight_decay,
+        train_split=train_split,
+        batch_size=batch_size,
+        callbacks=[
+            "accuracy",
+            ("lr_scheduler", LRScheduler('CosineAnnealingLR', T_max=n_epochs - 1)),
+        ],
+        device=device,
+    )
+
+    dataset = skorch.dataset.Dataset(X, Y)
+    print(X.shape)
+    dataset = skorch.dataset.Dataset(np.zeros((321, 2, 35, 801)), Y)
+
+    for repeat in range(1):
+        clf.fit(dataset, y=None, epochs=n_epochs)
+
+    _, test_dataset = train_split(dataset)
+
+    p = clf.predict(test_dataset.X)
+
+    report = metrics.classification_report(y_true=test_dataset.y,
+                                           y_pred=p)
+
+    print(report)
+    return report
+
+# %%
+
+
+def decode_subject(subject):
+    filenames = [e for e in Path('data').iterdir()
+                 if e.name.endswith('.dump')
+                 and e.name.startswith(subject_name_table[subject])
+                 and e.name.split('.')[-2] != '10']
+
+    filenames = sorted(filenames)[2:]
+
+    assert len(filenames) == 8, 'Invalid filenames {}'.format(filenames)
+    LOGGER.debug('Using the filenames: {}'.format(filenames))
+
+    X_raw, Y, G, ch_names = _get_x_y_g(filenames)
+    for mode in ['EEG', 'MEG']:
+        X = _select_mode(X_raw, ch_names, mode=mode)
+
+        X = _preprocess(X)
+
+        for valid_group in np.unique(G):
+            report = decoding(X, Y, G, valid_group)
+            break
+
+        break
+
+
+# %%
+for subject in subject_name_table:
+    decode_subject(subject)
+
+# %%
--- decoding/analysis.py
+++ decoding/analysis.py
@@ -2,68 +2,42 @@
 import mne
 import torch
 import numpy as np
-import pandas as pd
 
+from pathlib import Path
+from tqdm.auto import tqdm
 from sklearn import metrics
+
+from braindecode import EEGClassifier
 
 from braindecode.models import ShallowFBCSPNet
 from braindecode.models import to_dense_prediction_model
 from braindecode.models import get_output_shape
 
+from braindecode.training import CroppedLoss
+
 from braindecode.datasets import create_from_mne_epochs
+
+from braindecode.augmentation import AugmentedDataLoader, SignFlip
+from braindecode.augmentation import FrequencyShift
 
 from braindecode.preprocessing import exponential_moving_standardize
 from braindecode.preprocessing import preprocess
 from braindecode.preprocessing import Preprocessor
 from braindecode.preprocessing import scale
 
-from braindecode.augmentation import AugmentedDataLoader, SignFlip
-from braindecode.augmentation import FrequencyShift
-
 from skorch.callbacks import LRScheduler
 from skorch.helper import predefined_split
 
-from braindecode import EEGClassifier
-from braindecode.training import CroppedLoss
 
-from pathlib import Path
-from tqdm.auto import tqdm
+# %% --------------------------------------------------------------------------------
+# System setup
 
-from subjects_on_disk import mk_subject_table
-from known_folders import known_folders
-
-
-# %%
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(device)
+print('Using the {}'.format(device))
 
-# %%
-input_window_samples = 500
-n_chans = 35
-n_classes = 5
+# %% --------------------------------------------------------------------------------
+# User setup
 
-model = ShallowFBCSPNet(
-    n_chans,
-    n_classes,
-    input_window_samples=input_window_samples,
-    final_conv_length=10
-)
-
-model2 = ShallowFBCSPNet(
-    n_chans,
-    n_classes,
-)
-
-model, model2
-
-# %%
-
-to_dense_prediction_model(model)
-
-n_preds_per_input = get_output_shape(model, n_chans, input_window_samples)[2]
-n_preds_per_input
-
-# %%
 subject_name_table = dict(
     S01='meg-eeg-20220119',
     S02='MEG20220315',
@@ -71,193 +45,288 @@ subject_name_table = dict(
     S04='MEG20220329'
 )
 
-# %%
-subject = 'S01'
-
-filenames = [e for e in Path('data').iterdir()
-             if e.name.endswith('.dump.epoch')
-             and e.name.startswith(subject_name_table[subject])
-             and e.name.split('.')[-2] != '10']
-
-filenames = sorted(filenames)[2:]
-filenames
-
-# %%
-
-
-def get_epochs_list(filenames):
-    mag_epochs_list = []
-    for e in filenames:
-        epochs = mne.read_epochs(e).pick_types(meg=True)
-        epochs.events[:, -1] %= 5
-        mag_epochs_list.append(epochs)
-
-    eeg_epochs_list = []
-    for e in filenames:
-        epochs = mne.read_epochs(e).pick_types(eeg=True)
-        drop_ch_names = epochs.ch_names[35:]
-        epochs.drop_channels(drop_ch_names)
-        epochs.events[:, -1] %= 5
-        eeg_epochs_list.append(epochs)
-
-    print(len(mag_epochs_list), mag_epochs_list[0].get_data().shape)
-    print(len(eeg_epochs_list), eeg_epochs_list[0].get_data().shape)
-
-    return mag_epochs_list, eeg_epochs_list
-
-
-# %%
-# The shape of mag_epochs_list[0] is like
-# 40 x 273 x 1001
-
-# The shape of eeg_epochs_list[0] is like
-# 40 x 35 x 1001
-
-mag_epochs_list, eeg_epochs_list = get_epochs_list(filenames)
-
-# %%
-kwargs = dict(
-    window_size_samples=input_window_samples,
-    window_stride_samples=n_preds_per_input,
-    drop_last_window=False
-
-
-)
-mag_dataset = create_from_mne_epochs(mag_epochs_list, **kwargs)
-eeg_dataset = create_from_mne_epochs(eeg_epochs_list, **kwargs)
-
-# %%
-group = []
-event = []
-for grp, epochs in enumerate(eeg_epochs_list):
-    for evt in epochs.events:
-        group.append(grp)
-        event.append(evt[-1])
-
-description = dict(
-    event=event,
-    group=group
-)
-
-mag_dataset.set_description(description, overwrite=True)
-eeg_dataset.set_description(description, overwrite=True)
-mag_dataset.description, eeg_dataset.description
-
-# %%
-factor_new = 1e-3
-
-init_block_size = 200
-
-mag_preprocessors = [
-    Preprocessor(scale, factor=1e14, apply_on_array=True),
-    # Preprocessor(exponential_moving_standardize,
-    #              factor_new=factor_new, init_block_size=init_block_size)
-]
-
-eeg_preprocessors = [
-    Preprocessor(scale, factor=1e6, apply_on_array=True),
-    # Preprocessor(exponential_moving_standardize,
-    #              factor_new=factor_new, init_block_size=init_block_size)
-]
-
-
-preprocess(mag_dataset, mag_preprocessors)
-preprocess(eeg_dataset, eeg_preprocessors)
-
-# %%
-valid_group = 0
-
-
-def _map(e):
-    if e == valid_group:
-        return 'valid'
-    return 'train'
-
-
-dct = dict(
-    split=eeg_dataset.description['group'].map(_map)
-)
-
-eeg_dataset.set_description(dct, overwrite=True)
-
-splitted = eeg_dataset.split('split')
-
-train_set = splitted['train']
-valid_set = splitted['valid']
-
-len(train_set), len(valid_set)
-
-# %%
-n_classes = 5
+input_window_samples = 1001
+final_conv_length = 30
 sfreq = 200
 
-freq_shift = FrequencyShift(
-    probability=.5,
-    sfreq=sfreq,
-    max_delta_freq=2.  # the frequency shifts are sampled now between -2 and 2 Hz
-)
-
-sign_flip = SignFlip(probability=.3)
-
-transforms = [
-    freq_shift,
-    sign_flip
-]
-
-# %%
-input_window_samples = 500
-n_chans = 35
-n_classes = 5
-
-model = ShallowFBCSPNet(
-    n_chans,
-    n_classes,
+eeg_model_kwargs = dict(
+    n_chans=35,
+    n_classes=5,
     input_window_samples=input_window_samples,
-    final_conv_length=10
+    final_conv_length=final_conv_length,
 )
 
-# These values we found good for shallow network:
-lr = 0.0625 * 0.01
-weight_decay = 0
-
-# For deep4 they should be:
-# lr = 1 * 0.01
-# weight_decay = 0.5 * 0.001
-
-batch_size = 64
-n_epochs = 200
-
-clf = EEGClassifier(
-    model,
-    cropped=True,
-    iterator_train=AugmentedDataLoader,
-    iterator_train__transforms=transforms,
-    criterion=CroppedLoss,
-    criterion__loss_function=torch.nn.functional.nll_loss,
-    optimizer=torch.optim.AdamW,
-    train_split=predefined_split(valid_set),
-    optimizer__lr=lr,
-    optimizer__weight_decay=weight_decay,
-    iterator_train__shuffle=True,
-    batch_size=batch_size,
-    callbacks=[
-        "accuracy", ("lr_scheduler", LRScheduler(
-            'CosineAnnealingLR', T_max=n_epochs - 1)),
-    ],
-    device=device,
+mag_model_kwargs = dict(
+    n_chans=273,
+    n_classes=5,
+    input_window_samples=input_window_samples,
+    final_conv_length=final_conv_length,
 )
-# Model training for a specified number of epochs. `y` is None as it is already supplied
-# in the dataset.
+
+# %% --------------------------------------------------------------------------------
+
+
+def mk_model(model_kwargs, return_n_preds=False):
+    '''
+    Make a brand new model using the model_kwargs.
+    If return_n_preds is True, the aim is to get the n_preds_per_input.
+    The meaning is in the document of
+    https://braindecode.org/auto_examples/plot_bcic_iv_2a_moabb_cropped.html#
+    '''
+    kwargs = model_kwargs.copy()
+
+    n_chans = kwargs.pop('n_chans')
+    n_classes = kwargs.pop('n_classes')
+
+    model = ShallowFBCSPNet(
+        n_chans,
+        n_classes,
+        **kwargs,
+    )
+
+    if return_n_preds:
+        to_dense_prediction_model(model)
+        n_preds_per_input = get_output_shape(
+            model, n_chans, input_window_samples)[2]
+        return model, n_preds_per_input
+
+    return model
+
+
+# It requires dry-run to compute the n_preds_per_input as a startup
+model, n_preds_per_input = mk_model(eeg_model_kwargs, return_n_preds=True)
+n_preds_per_input
+
+
+def mk_transforms(sfreq=sfreq):
+    '''
+    Make the transforms for the model training.
+    '''
+    freq_shift = FrequencyShift(
+        probability=.5,
+        sfreq=sfreq,
+        max_delta_freq=2.  # the frequency shifts are sampled now between -2 and 2 Hz
+    )
+
+    sign_flip = SignFlip(probability=.3)
+
+    transforms = [
+        freq_shift,
+        sign_flip
+    ]
+
+    return transforms
+
+
+def mk_clf(model, transforms, n_epochs, train_split):
+    '''
+    Make the clf from the model and transforms.
+
+    !!! The classifier is highly customized for model setup and play method,
+    !!! So, make sure you check and change this before use.
+    '''
+
+    # These values we found good for shallow network:
+    lr = 0.0625 * 0.01
+    weight_decay = 0
+
+    # For deep4 they should be:
+    # lr = 1 * 0.01
+    # weight_decay = 0.5 * 0.001
+
+    batch_size = 64
+
+    clf = EEGClassifier(
+        model,
+        cropped=True,
+        iterator_train=AugmentedDataLoader,
+        iterator_train__transforms=transforms,
+        criterion=CroppedLoss,
+        criterion__loss_function=torch.nn.functional.nll_loss,
+        optimizer=torch.optim.AdamW,
+        train_split=train_split,
+        optimizer__lr=lr,
+        optimizer__weight_decay=weight_decay,
+        iterator_train__shuffle=True,
+        batch_size=batch_size,
+        callbacks=[
+            "accuracy", ("lr_scheduler", LRScheduler(
+                'CosineAnnealingLR', T_max=n_epochs - 1)),
+        ],
+        device=device,
+    )
+
+    return clf
+
+
+def read_subject(subject, input_window_samples=input_window_samples, window_stride_samples=n_preds_per_input):
+    '''
+    Read the subject's file.
+
+    And convert them into the dataset.
+
+    Returns:
+        - mag_dataset, eeg_dataset
+    '''
+
+    # ----
+    # File names
+    filenames = [e for e in Path('data').iterdir()
+                 if e.name.endswith('.dump.epoch')
+                 and e.name.startswith(subject_name_table[subject])
+                 and e.name.split('.')[-2] != '10']
+
+    filenames = sorted(filenames)[2:]
+
+    # ----
+    # Make two lists of epochs
+    # - One for MAG
+    # - Another for EEG
+
+    def get_epochs_list(filenames):
+        mag_epochs_list = []
+        for e in filenames:
+            epochs = mne.read_epochs(e).pick_types(meg=True)
+            epochs.events[:, -1] %= 5
+            mag_epochs_list.append(epochs)
+
+        eeg_epochs_list = []
+        for e in filenames:
+            epochs = mne.read_epochs(e).pick_types(eeg=True)
+            drop_ch_names = epochs.ch_names[35:]
+            epochs.drop_channels(drop_ch_names)
+            epochs.events[:, -1] %= 5
+            eeg_epochs_list.append(epochs)
+
+        print(len(mag_epochs_list), mag_epochs_list[0].get_data().shape)
+        print(len(eeg_epochs_list), eeg_epochs_list[0].get_data().shape)
+
+        return mag_epochs_list, eeg_epochs_list
+
+    # The shape of mag_epochs_list[0] is like
+    # 40 x 273 x 1001
+
+    # The shape of eeg_epochs_list[0] is like
+    # 40 x 35 x 1001
+
+    # mag_epochs_list, eeg_epochs_list = get_epochs_list(filenames[:2])
+    mag_epochs_list, eeg_epochs_list = get_epochs_list(filenames)
+
+    # ----
+    # Make dataset for MAG and EEG data
+    kwargs = dict(
+        window_size_samples=input_window_samples,
+        window_stride_samples=window_stride_samples,
+        drop_last_window=False
+    )
+    mag_dataset = create_from_mne_epochs(mag_epochs_list, **kwargs)
+    eeg_dataset = create_from_mne_epochs(eeg_epochs_list, **kwargs)
+
+    # ----
+    # Update the description
+    group = []
+    event = []
+    for grp, epochs in enumerate(eeg_epochs_list):
+        for evt in epochs.events:
+            group.append(grp)
+            event.append(evt[-1])
+
+    description = dict(
+        event=event,
+        group=group
+    )
+
+    # ! The set_description raw code is changed to make it work
+    mag_dataset.set_description(description, overwrite=True)
+    eeg_dataset.set_description(description, overwrite=True)
+    mag_dataset.description, eeg_dataset.description
+
+    return mag_dataset, eeg_dataset
+
+
+def preprocess_dataset(dataset, factor):
+    '''
+    Preprocess the dataset **IN-PLACE**.
+
+    The default parameter is
+
+    - MAG: factor = 1e14
+    - EEG: factor = 1e6
+    '''
+
+    preprocessors = [
+        Preprocessor(scale, factor=factor, apply_on_array=True),
+    ]
+
+    preprocess(dataset, preprocessors)
+
+    return
+
+
+def split_dataset(dataset, valid_group=0):
+    '''
+    Split the dataset into 'train' and 'valid' set.
+
+    The description of the dataset will be changed accordingly.
+    The set label has the column of 'split'.
+    '''
+
+    def _map(e):
+        if e == valid_group:
+            return 'valid'
+        return 'train'
+
+    dct = dict(
+        split=dataset.description['group'].map(_map)
+    )
+
+    dataset.set_description(dct, overwrite=True)
+
+    splitted = dataset.split('split')
+
+    train_set = splitted['train']
+    valid_set = splitted['valid']
+
+    print('Split the dataset into train ({}) and valid ({}) set'.format(
+        len(train_set), len(valid_set)))
+
+    return train_set, valid_set
+
+
+# %% --------------------------------------------------------------------------------
+# Setup the subject name
+subject = 'S01'
+
+n_epochs = 500
+
+# Read files and make dataset
+mag_dataset, eeg_dataset = read_subject(subject)
+
+# Preprocess dataset
+preprocess_dataset(mag_dataset, 1e14)
+preprocess_dataset(eeg_dataset, 1e6)
+
+# %% --------------------------------------------------------------------------------
+# EEG dataset
+# Split the dataset
+train_set, valid_set = split_dataset(eeg_dataset)
+
+# Make the clf
+transforms = mk_transforms()
+model = mk_model(eeg_model_kwargs)
+clf = mk_clf(model, transforms, n_epochs, predefined_split(valid_set))
+
+# Model training for a specified number of epochs.
+# `y` is None as it is already supplied in the dataset.
 clf.fit(train_set, y=None, epochs=n_epochs)
 
-# %%
+# %% --------------------------------------------------------------------------------
 y = clf.predict(valid_set)
-# %%
-y
-# %%
+
 y_true = np.array([e[1] for e in valid_set])
 y_pred = clf.predict(valid_set)
 
 report = metrics.classification_report(y_true=y_true, y_pred=y_pred)
 print(report)
+
 # %%
